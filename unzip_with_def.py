46556a26b0e0e7310314ba33be9ffd70ed8f73fe
noter28 <noterfield@gmail.com>
2019-02-08 17:17:39 +0200

    from mac

--- unzip_with_def.py
+++ unzip_with_def.py
@@ -2,36 +2,38 @@
 import shutil
 import os, sys
 import zipfile
-directory =         os.path.join('D:/','All_photos','20181215') #first source
-outcome_directory = directory + r'_unzip'     #output manually cropping photo
+directory =         os.path.join('/Users','olehpryshliak','Documents','All_photos','20190103') #first source
+outcome_directory = directory + r'_unzip'
 just_photo = directory + r'_photo'
 files = os.listdir(directory) #list of item in directory
-#a=0
+#a=0 /Users/olehpryshliak/Documents/All_photos/20190123
+#     'Users/olehpryshliak/Documents/All_photos/20190123'
 try:
-    nf1 = os.makedirs(outcome_directory)
-    nf2 = os.makedirs(just_photo)
+    os.makedirs(outcome_directory)
+    os.makedirs(just_photo)
     print('Folder(s) are creted')
 except FileExistsError:
     print('Folder(s) already were creted')
     pass
 def zip(directory, outcome_directory, files):
     for i in files:
+        if not i.startswith('.'):
         #TRY, DIDN'T CHECK!!!!!!
-        try:
-            z = zipfile.ZipFile(directory+'\\'+i, 'r')
+            z = zipfile.ZipFile(directory+'//'+i, 'r')
             z.extractall(outcome_directory)
-        except Exception:
-            print(i)
-        c=os.listdir(outcome_directory)  #список папок внутри которых фотки и json
+            c=os.listdir(outcome_directory)  #список папок внутри которых фотки и json
     print('All files are extracted')
     return c
 def move(outcome_directory, just_photo):
     for i in zip(directory, outcome_directory, files):
-        v = os.listdir(outcome_directory + '\\' + i)
-        shutil.move(outcome_directory + '\\' + i + '\\' + v[2], just_photo)
+        v = os.listdir(os.path.join(outcome_directory,i))
+        for item in v:
+            if item.endswith('obj.0.1.jpg'):
+                shutil.move(outcome_directory + '//' + i + '//' + item, just_photo)
 #zip(directory, outcome_directory, files)
 try:
     move(outcome_directory, just_photo)
     print('Done')
 except Exception:
     print('Some error')
+    pass
